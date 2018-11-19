885e916a75a8d70ebc1e53506b95d49469e910a3
ValdenizeRibeiro <valdenizesilva8@gmail.com>
2018-11-19 11:00:45 -0200

    crud de empresa
    controller para gerir os cruds das tabelas, crud empresa;

---
+++ app/Http/Controllers/CrudController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class CrudController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('index');
+    }
+
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request,$name)
+    {
+        return app('App\Http\Controllers'.'\\'.$name.'Controller')->store($request);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($name, $id=null,$variable=null)
+    {
+        return app('App\Http\Controllers'.'\\'.$name.'Controller')->show($id,$variable);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update($name,Request $request, $id=null)
+    {
+        return app('App\Http\Controllers'.'\\'.$name.'Controller')->update($request,$id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($name,$id=null)
+    {
+        return app('App\Http\Controllers'.'\\'.$name.'Controller')->destroy($id);
+    }
+}
---
+++ app/Http/Controllers/EmpresaController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Empresa;
+use App\Models\Endereco;
+use App\Models\Telefone;
+use App\Models\EmpresaHasEndereco as HasE;
+use App\Models\TelefoneHasEmpresa as HasT;
+
+
+class EmpresaController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $empresa=Empresa::inserir($request->all());
+        return response()->json($empresa);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id,$variable)
+    {
+        if ($id == null) {
+            $empresa = Empresa::all();
+            return response()->json($empresa);       
+        }
+        $empresaOne = Empresa::ler($id,$variable);
+        return response()->json($empresaOne); 
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        if ($id != null) {
+            $empresaOne=Empresa::alterar($id,$request->all());
+            return response()->json($empresaOne);       
+        }else{
+            return '401';
+        }   
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        if ($id != null) {
+            $empresaOne = Empresa::excluir($id);
+            return response()->json($empresaOne);;       
+        }else{
+            return '401';
+        }
+    }
+}
